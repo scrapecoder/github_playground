391999bf41e7ed962cfaa60cd327347eadac2c82
Sourav_sps <sourav_kukreti@softprodigy.com>
2025-11-18 23:41:08 +0530

    Revert "line 4"
    This reverts commit 76ff127524eb3b92e45749a7b44b7f43dc3b64d5.

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 //this one is app.js
 console.log("Hllo github from app.js");
 console.log("hello this from line 3");
-console.log("hello this from line 4");
-console.log("hello this from line 5");
-console.log("hello this from revert");
+console.log("hello this from line 3");

e9a524afed90cf8877411c26338a4a7659d79ab0
Sourav_sps <sourav_kukreti@softprodigy.com>
2025-11-18 23:51:35 +0530

    app.js

--- app.js
+++ app.js
@@ -2,5 +2,4 @@
 console.log("Hllo github from app.js");
 console.log("hello this from line 3");
 console.log("hello this from line 3");
-console.log("fix for the day");
-//yes its working now
+console.log("hello this from line 4");
